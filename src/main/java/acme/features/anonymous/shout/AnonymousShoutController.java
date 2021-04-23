8b4609f6e7b18864ce062244272c52803502500d
dantorval-us <dantorval@alum.us.es>
2021-04-24 01:21:03 +0200

    Añade filtro para shouts y los ordena
    Ahora se puede filtrar el listado de shouts de modo que solo muestre
    los del último mes, además de mostrarlos de más a menos recientes.

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
@@ -1,3 +1,4 @@
+
 package acme.features.anonymous.shout;
 
 import javax.annotation.PostConstruct;
@@ -6,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import acme.components.CustomCommand;
 import acme.entities.shouts.Shout;
 import acme.framework.components.BasicCommand;
 import acme.framework.controllers.AbstractController;
@@ -13,22 +15,28 @@ import acme.framework.entities.Anonymous;
 
 @Controller
 @RequestMapping("/anonymous/shout")
-public class AnonymousShoutController extends AbstractController<Anonymous, Shout>{
-	
+public class AnonymousShoutController extends AbstractController<Anonymous, Shout> {
+
 	// Internal state --------------------------------------------------------
-	
+
 	@Autowired
-	private AnonymousShoutCreateService createService;
-  
-  @Autowired
-	private AnonymousShoutListService listService;
-	
+	private AnonymousShoutCreateService			createService;
+
+	@Autowired
+	private AnonymousShoutListService			listService;
+
+	@Autowired
+	private AnonymousShoutListLastMonthService	listLastMonthService;
+
 	// Constructors ----------------------------------------------------------
-	
+
+
 	@PostConstruct
 	private void initialise() {
 		super.addBasicCommand(BasicCommand.CREATE, this.createService);
-    super.addBasicCommand(BasicCommand.LIST, this.listService);
+		super.addBasicCommand(BasicCommand.LIST, this.listService);
+		super.addCustomCommand(CustomCommand.LIST_LAST_MONTH, BasicCommand.LIST, this.listLastMonthService);
+
 	}
-	
+
 }
