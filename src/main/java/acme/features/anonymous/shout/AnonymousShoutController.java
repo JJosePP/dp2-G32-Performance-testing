73f8103c76419e796875fde182466ff392d949e2
dantorval-us <dantorval@alum.us.es>
2021-04-27 13:12:34 +0200

    Lista únicamente los shouts del último mes
    Antes, un anónimo tenía la opción de listar todos los shouts o
    solo los del último mes, tras el feedback del profesor, solo
    hace lo segundo. Además, los enlaces favoritos que no eran de
    este engtregable ya están eliminados.

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import acme.components.CustomCommand;
 import acme.entities.shouts.Shout;
 import acme.framework.components.BasicCommand;
 import acme.framework.controllers.AbstractController;
@@ -20,23 +19,17 @@ public class AnonymousShoutController extends AbstractController<Anonymous, Shou
 	// Internal state --------------------------------------------------------
 
 	@Autowired
-	private AnonymousShoutCreateService			createService;
+	private AnonymousShoutCreateService	createService;
 
 	@Autowired
-	private AnonymousShoutListService			listService;
-
-	@Autowired
-	private AnonymousShoutListLastMonthService	listLastMonthService;
+	private AnonymousShoutListService	listService;
 
 	// Constructors ----------------------------------------------------------
-
 
 	@PostConstruct
 	private void initialise() {
 		super.addBasicCommand(BasicCommand.CREATE, this.createService);
 		super.addBasicCommand(BasicCommand.LIST, this.listService);
-		super.addCustomCommand(CustomCommand.LIST_LAST_MONTH, BasicCommand.LIST, this.listLastMonthService);
-
 	}
 
 }
